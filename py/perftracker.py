9a9e7128580aaff17ed605d3d386647b6c3cf3af
Fernando Silveira <fsilveira@gmail.com>
2011-06-07 17:38:47 -0300

    Added "plot_once", "plot_every" and "plot_rising" functions.

--- py/perftracker.py
+++ py/perftracker.py
@@ -3,7 +3,7 @@
 from pdb import set_trace
 import struct
 from numpy import array
-from pylab import plot, xlabel, ylabel, title, legend, show
+from pylab import * #plot, xlabel, ylabel, title, legend, show
 
 class TrackPoint():
         def __init__(self, filename, line, function, entries):
@@ -12,68 +12,114 @@ class TrackPoint():
                 self.function = function
                 self.entries = entries
 
-class Track():
-        def eat_point(self, file):
-                ( filename, line, function, n ) = struct.unpack('256sI256sI', file.read(520))
-                filename = filename.rstrip("\x00")
-                function = function.rstrip("\x00")
+def eat_point(file):
+        ( filename, line, function, n ) = struct.unpack('256sI256sI', file.read(520))
+        filename = filename.rstrip("\x00")
+        function = function.rstrip("\x00")
 
-                #print filename + ':' + str(line) + '; n = ' + str(n) + ';'
-                n = n * 2
+        #print filename + ':' + str(line) + '; n = ' + str(n) + ';'
+        n = n * 2
 
-                arr = struct.unpack(str(n) + 'd', file.read(n * 8))
+        arr = struct.unpack(str(n) + 'd', file.read(n * 8))
 
-                point = TrackPoint(filename, line, function, array(arr).reshape(-1, 2))
+        point = TrackPoint(filename, line, function, array(arr).reshape(-1, 2))
 
-                return point
+        return point
 
-        def read_file(self, filename):
-                #print '' + filename + ':'
-                file = open(filename, 'rb')
+def read_file(filename):
+        #print '' + filename + ':'
+        file = open(filename, 'rb')
 
-                pts = []
+        points = []
 
-                try:
-                        while True:
-                                pt = self.eat_point(file)
-                                pts.append(pt)
-                                #print pt.function + ';'
+        try:
+                while True:
+                        pt = eat_point(file)
+                        points.append(pt)
+                        #print pt.function + ';'
 
-                except struct.error:
-                        pass
+        except struct.error:
+                pass
 
-                self.points = pts
+        return points
 
-class TrackAnalyzer():
-        def __init__(self, track):
-                self.track = track
+def plot_once(points):
+        l = []
+        for p in points:
+                plot(p.entries[:, 0], p.entries[:, 1])
+                l.append(p.function)
+        if len(l) > 0:
+                xlabel('instant')
+                ylabel('spent')
+                legend(l)
+                show()
 
-        def plot_all(self):
-                n = -1
-                for p in self.track.points:
-                        if n < 0 or n > 512:
-                                if n >= 0:
-                                        legend(l)
-                                        show()
-                                        #pass
-                                n = 0
-                                l = []
-                        plot(p.entries[:, 0], p.entries[:, 1])
-                        l.append(p.function)
-                        n = n + 1
+def plot_every(points):
+        for p in points:
+                xlabel('instant')
+                ylabel('spent')
+                title(p.function)
+                plot(p.entries[:, 0], p.entries[:, 1])
+                plot(p.entries[:, 0], p.entries[:, 1], 'o')
+                show()
 
-def read_track_file(filename):
-        t = Track()
-        t.read_file(filename)
-        return t
+def plot_rising(points):
+        for p in points:
+                arr = p.entries
 
-def plot_track(track):
-        ta = TrackAnalyzer(track)
-        ta.plot_all()
-        show()
+                if len(arr) < 50:
+                        continue
 
-o = read_track_file('perftracker.out-2')
-#n = read_track_file('perftracker~improved.out')
+                middle = (arr[-1, 0] - arr[0, 0]) / 2 + arr[0, 0]
+                left = nonzero(arr[:, 0] <= middle)
+                right = nonzero(arr[:, 0] > middle)
+
+                arr_l = (arr[left, 1])[0]
+                arr_r = (arr[right, 1])[0]
+
+                left_p = sum(arr_l) / len(arr_l)
+                right_p = sum(arr_r) / len(arr_r)
+
+                if left_p >= right_p:
+                        continue
+
+                #print str(left_p) + ' vs ' + str(right_p)
+                print p.function
+                title(p.function)
+                xlabel('instant')
+                ylabel('spent')
+                plot(arr[:, 0], arr[:, 1])
+                plot(arr[:, 0], arr[:, 1], 'o')
+                show()
+
+
+pts = read_file('perftracker.out-2')
+
+set_trace()
+
+plot_once(pts)
+plot_every(pts)
+plot_rising(pts)
+
+
+#class TrackAnalyzer():
+#        def __init__(self, track):
+#                self.track = track
+#
+#        def plot_all(self):
+#                n = -1
+#                for p in self.track.points:
+#                        if n < 0 or n > 512:
+#                                if n >= 0:
+#                                        legend(l)
+#                                        show()
+#                                        #pass
+#                                n = 0
+#                                l = []
+#                        plot(p.entries[:, 0], p.entries[:, 1])
+#                        l.append(p.function)
+#                        n = n + 1
+
 
 #tr = Track()
 #tr.read_file('perftracker.out')
@@ -81,15 +127,6 @@ o = read_track_file('perftracker.out-2')
 #ta = TrackAnalyzer(tr)
 #ta.plot_all()
 
-for p in o.points:
-        print p.filename + ':' + str(p.line) + ': ' + p.function
-        plot(p.entries[:, 0], p.entries[:, 1])
-        plot(p.entries[:, 0], p.entries[:, 1], 'ro')
-        show()
-
-plot_track(o)
-
-set_trace()
 
 #try:
 #        while True:
