3e1b190df3312115685b5493076e9ee5651eb1e7
Andre de Oliveira <deoliveirambx@googlemail.com>
2015-05-20 16:12:51 -0300

    Make DOSOMETHING() actually "do something", since usleep() cant be considered when doing getrusage()

--- test/test0.cpp
+++ test/test0.cpp
@@ -1,17 +1,22 @@
+#include <sys/param.h>
+#include <sys/stat.h>
+
 #include <err.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #include <iostream>
 #include <iomanip>
 
-#include <perftracker.hpp>
+#include "perftracker.hpp"
+
+void	dosomething(uint, uint);
 
 #if 1
 #define DOSOMETHING(x, y) do { \
-	useconds_t __us = x * 10000 + y * 1000; \
-	std::cerr << " " << std::setw(10) << std::left << __func__ << std::right << " " << std::setw(10) << (static_cast<unsigned int>(__us) / 1000) << std::endl; \
-	::usleep(__us); \
-} while (0)
+		dosomething(__func__, x, y); \
+	} while (0)
 #else
 #define DOSOMETHING(x, y)
 #endif
@@ -22,12 +27,38 @@ tracker tracker::instance;
 
 static unsigned long next = 1;
 
+void
+dosomething(const char *func, uint x, uint y)
+{
+	uint		i;
+	int		fd;
+	char		filename[MAXPATHLEN];
+	uint		__us = x * y;
+
+	std::cerr << " " << std::setw(10) << std::left << func
+	    << std::right << " " << std::setw(10)
+	    << (static_cast<unsigned int>(__us) / 1000) << std::endl;
+
+	i = ::arc4random_uniform(__us);
+
+	while (i--) {
+		snprintf(filename, sizeof(filename), "/tmp/.test%0d", i);
+		if ((fd = open(filename, O_RDWR | O_CREAT | O_APPEND)) == -1)
+			err(1, "open");
+
+		if (fchmod(fd, S_IRWXU | S_IRWXG | S_IRWXO) == -1)
+			err(1, "fchmod");
+
+		write(fd, "test\n", sizeof("test"));
+		close(fd);
+	}
+}
+
 unsigned int
 myrand(void) {
 	next = next * 1103515245 + 12345;
 	return (unsigned int)(next / 32) % 16;
 }
-
 
 void
 test0(unsigned int n)
@@ -142,11 +173,8 @@ test15(unsigned int n)
 }
 
 int
-main(int argc, char *argv[])
+main(void)
 {
-	if (argc != 1)
-		errx(EXIT_FAILURE, "invalid number of arguments");
-
 	typedef void (*fn)(unsigned int);
 
 	fn functions[16];
@@ -180,7 +208,7 @@ main(int argc, char *argv[])
 	unsigned int max = 1024;
 	for (unsigned int n = 0; n < max; n++) {
 		std::cerr << " " << std::setw(10) << std::fixed << std::showpoint << std::setprecision(2) << static_cast<double>(n) / (max - 1) * 100 << "% " << " " << std::setw(10) << n << " ";
-		(functions[rand() % 16])(rand() % 16);
+		(functions[rand() % 16])(arc4random() % 16);
 	}
 
 	PT_DUMP();
